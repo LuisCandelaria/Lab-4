5bf903445f0be63bf2371ea290d63f9e655b3821
Luis Eugenio Candelaria Azpilcueta <luiseugenio.candelaria@gmail.com>
2020-03-11 19:17:19 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1>
+    Laboratory 4 - “The amazing calculator” 11-mar-20
+</h1>
+<h3>
+    Luis Eugenio Candelaria Azpilcueta A00816826
+</h3>
+<br>
+<h4>
+    Description:
+</h4>
+<p>
+    For this laboratory you will have to add functionality to the basic calculator that is provided in the zip file under the laboratories description.
+    <br>
+1) It is a basic calculator. You must add functionality to the buttons of RESET, =, +, -, *, /. Each button must attempt the operation that it’s selected. Every time that any button is clicked the input for numbers must be clean.
+<br>
+2) There is an input where you can type numbers. Those numbers are the ones that will be calculated with the operators described in step 1).
+<br>
+3) You must keep track of the result and display it on the RESULT = input when the user hits the = button.
+<br>
+4) The button of RESET will clean both your result and the input for numbers.
+<br>
+5) There is a text area at the bottom of the calculator. This text area is meant for keeping a log of all the operations done in the calculator.
+</p>
---
+++ js/index.js
@@ -0,0 +1,136 @@
+let resultTxtBox = document.getElementById("resultValue");
+let Txthistory = document.getElementById("logInformation");
+let input = document.getElementById("inputNum");
+let result = 0;
+let history = "";
+let flagadd = false;
+let flagsub = false;
+let flagmul = false;
+let flagdiv = false;
+
+function getResult(){
+    if(flagadd){
+        result += Number(input.value);
+        history += input.value + "=" + String(result);
+        resultTxtBox.value = String(result);
+    }
+    if(flagsub){
+        result -= Number(input.value);
+        history += input.value + "=" + String(result);
+        resultTxtBox.value = String(result);
+    }
+    if(flagmul){
+        result *= Number(input.value);
+        history += input.value + "=" + String(result);
+        resultTxtBox.value = String(result);
+    }
+    if(flagdiv){
+        result /= Number(input.value);
+        history += input.value + "=" + String(result);
+        resultTxtBox.value = String(result);
+    }
+    Txthistory.textContent += history + "\n";
+    afterResult();
+}
+
+function check(){
+    if(flagsub){
+        result -= Number(input.value);
+    }
+    if(flagmul){
+        result *= Number(input.value);
+    }
+    if(flagdiv){
+        result /= Number(input.value);
+    }
+    if(flagadd){
+        result += Number(input.value);
+    }
+}
+
+function firstop(operation){
+    history += input.value + operation;
+    result = Number(input.value);
+    input.value = "";
+    resultTxtBox = "";
+}
+
+function sum(){
+    if(result == 0){
+        firstop("+");
+        flagadd = true;
+    }
+    else{
+        check();
+        history += input.value + "+";
+        flagadd = true;
+        flagsub = false;
+        flagmul = false;
+        flagdiv = false;
+        input.value = "";
+    }
+}
+
+function sub(){
+    if(result == 0){
+        firstop("-");
+        flagsub = true;
+    }
+    else{
+        check();
+        history += input.value + "-";
+        flagsub = true;
+        flagadd = false;
+        flagmul = false;
+        flagdiv = false;
+        input.value = "";
+    }
+}
+
+function mult(){
+    if(result == 0){
+        firstop("*");
+        flagmul = true;
+    }
+    else{
+        check();
+        history += input.value + "*";
+        flagmul = true;
+        flagsub = false;
+        flagadd = false;
+        flagdiv = false;
+        input.value = "";
+    }
+}
+
+function div(){
+    if(result == 0){
+        firstop("/");
+        flagdiv = true;
+    }
+    else{
+        check();
+        history += input.value + "/";
+        flagdiv = true;
+        flagsub = false;
+        flagmul = false;
+        flagadd = false;
+        input.value = "";
+    }
+}
+
+function afterResult(){
+    input.value = "";
+    history = "";
+    result = 0;
+    flagadd = false;
+    flagsub = false;
+    flagmul = false;
+    flagdiv = false;
+}
+
+function resetCal(){
+    resultTxtBox.value = "";
+    Txthistory.innerText = "";
+    afterResult();
+}
